8bf54029fa0ca95352fd749772034545bdfeebbf
Feildmaster <admin@feildmaster.com>
2011-07-17 19:47:51 -0500

    - Added Piston Support - Updated to RB 1000

--- src/feildmaster/PermaFence/BreakListen.java
+++ src/feildmaster/PermaFence/BreakListen.java
@@ -7,32 +7,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPistonExtendEvent;
+import org.bukkit.event.block.BlockPistonRetractEvent;
 
 class BreakListen extends BlockListener {    
     public void onBlockBreak(BlockBreakEvent event) {
         if(event.getBlock().getType() == Material.FENCE) {
             Player player = event.getPlayer();
-            if(player.isOp() && player.getItemInHand().getType() == Material.GOLD_PICKAXE) {
+            if(player.isOp() && player.getItemInHand().getType() == Material.GOLD_PICKAXE)
                 event.setCancelled(false);
-            } else if(!event.isCancelled()) {
+            else if(!event.isCancelled())
                 event.setCancelled(true);
-            }
         }
     }
 
     public void onBlockIgnite(BlockIgniteEvent event) {
-        if(next2Fence(event.getBlock())) {
+        if(next2Fence(event.getBlock()))
             event.setCancelled(true);
-        }
+    }
+
+    public void onBlockPistonExtend(BlockPistonExtendEvent event) {
+        for(Block block : event.getBlocks())
+            if(block.getType() == Material.FENCE)
+                event.setCancelled(true);
+    }
+
+    public void onBlockPistonRetract(BlockPistonRetractEvent event) {
+        if(event.getBlock().getRelative(event.getDirection(),2).getType() == Material.FENCE)
+            event.setCancelled(true);
     }
 
     private Boolean next2Fence(Block block) {
-        if(block.getFace(BlockFace.UP).getType() == Material.FENCE
-                || block.getFace(BlockFace.DOWN).getType() == Material.FENCE
-                || block.getFace(BlockFace.NORTH).getType() == Material.FENCE
-                || block.getFace(BlockFace.SOUTH).getType() == Material.FENCE
-                || block.getFace(BlockFace.EAST).getType() == Material.FENCE
-                || block.getFace(BlockFace.WEST).getType() == Material.FENCE) return true;
+        if(block.getRelative(BlockFace.UP).getType() == Material.FENCE
+                || block.getRelative(BlockFace.DOWN).getType() == Material.FENCE
+                || block.getRelative(BlockFace.NORTH).getType() == Material.FENCE
+                || block.getRelative(BlockFace.SOUTH).getType() == Material.FENCE
+                || block.getRelative(BlockFace.EAST).getType() == Material.FENCE
+                || block.getRelative(BlockFace.WEST).getType() == Material.FENCE) return true;
         return false;
     }
 }
--- src/feildmaster/PermaFence/PermaFence.java
+++ src/feildmaster/PermaFence/PermaFence.java
@@ -12,6 +12,8 @@ public class PermaFence extends JavaPlugin {
     public void onEnable() {
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvent(Event.Type.BLOCK_BREAK, blockListener, Event.Priority.Highest, this);
+        pm.registerEvent(Event.Type.BLOCK_PISTON_EXTEND, blockListener, Event.Priority.Highest, this);
+        pm.registerEvent(Event.Type.BLOCK_PISTON_RETRACT, blockListener, Event.Priority.Highest, this);
         pm.registerEvent(Event.Type.BLOCK_IGNITE, blockListener, Event.Priority.Lowest, this);
         pm.registerEvent(Event.Type.ENTITY_EXPLODE, entityListener, Event.Priority.Lowest, this);
         System.out.println("PermaFence v"+getDescription().getVersion()+": Enabled. Fences are invincible!");
